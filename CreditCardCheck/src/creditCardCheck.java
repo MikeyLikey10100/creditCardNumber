e049b658c79b1771c8783150c4a3a47236429bec
michael.Nguyen <michael.Nguyen@MU-C1-18.mullen.pvt.k12.co.us>
2019-11-14 11:57:14 -0700

    creditCard

---
+++ CreditCardCheck/src/creditCardCheck.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class creditCardCheck
+	{
+
+		public static void main(String[] args) throws FileNotFoundException
+			{
+				Scanner file = new Scanner(new File ("creditCardNumbers.txt"));
+				
+				int counter = 0; 
+				
+				String [] numbers = new String;
+				while(file.hasNext()) 
+				{
+					
+		        }   
+
+
+			}
+
+
+
+	}
+
+
